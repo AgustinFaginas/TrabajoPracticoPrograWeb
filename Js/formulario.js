4741f1bb540b5025339c21ad999ff949e2570479
Pewmafe <matias-leandro@outlook.com>
2019-11-25 19:26:44 -0300

    responsive de pag formulario

--- Js/formulario.js
+++ Js/formulario.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
     //clonar
-    $(".contenedorCampos .clonar").click(function(){
-    	var clonar=$(this).parent(".contenedorCampos").clone(true); //clonar
-        $(this).parent(".contenedorCampos").after(clonar); //mostrar
+    $(".clonar").click(function(){
+    	var clonar=$(this).parents(".contenedorCampos").clone(true); //clonar
+        $(this).parent(".div-clonar-eliminar").parent(".contenedorCampos").after(clonar); //mostrar
     });
-        
-    
+
+
     //remover
-    $(".contenedorCampos .eliminar").click(function(){
-    	$(this).parent(".contenedorCampos").remove();
+    $(".eliminar").click(function(){
+    	$(this).parents(".contenedorCampos").remove();
     });
 });
--- Js/funciones.js
+++ Js/funciones.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    	
+
 			$(".content-tab:not(:eq(0))").toggle();
 			$(".title-tab i").toggleClass("fa-plus");
 
@@ -8,24 +8,23 @@ $(document).ready(function(){
 				$(".title-tab i").removeClass("fa-minus");
 				$(".title-tab i").addClass("fa-plus");
 				$(this).next().show();
-				$(this).children().addClass("fa-minus");				
+				$(this).children().addClass("fa-minus");
 			})
-		
-    $(".desplegable").toggle(
+
+      $(".desplegable").toggle(
           function() {
-            $(".seccion-header").parent().find(".nav-menu").slideDown('');
+            $(this).parents().find(".nav-menu").slideDown('');
           },
           function() {
-            $(".seccion-header").parent().find(".nav-menu").slideUp('');
+            $(this).parents().find(".nav-menu").slideUp('');
           }
         );
-$(window).resize(function(){
-    if($(this).width()>768){
-        $(".seccion-header").parent().find(".nav-menu").show();
-    } else{
-        $(".seccion-header").parent().find(".nav-menu").hide();
-    }
+        $(window).resize(function(){
+          if($(this).width()>768){
+            $(".desplegable").parents().find(".nav-menu").show();
+          } else{
+            $(".desplegable").parents().find(".nav-menu").hide();
+          }
 });
 
 });
-	
