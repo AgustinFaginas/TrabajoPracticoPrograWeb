65479066a63763430aeea8800d305b66cf048f3b
AgustinFaginas <agustin.faginas@hotmail.com>
2019-11-25 15:18:27 -0300

    Se hizo css y botones de pag detalle curso

--- Js/funciones.js
+++ Js/funciones.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    	
+			$(".content-tab:not(:eq(0))").toggle();
+			$(".title-tab i").toggleClass("fa-plus");
+
+			$(".title-tab").click(function(){
+				$(".content-tab").hide();
+				$(".title-tab i").removeClass("fa-minus");
+				$(".title-tab i").addClass("fa-plus");
+				$(this).next().show();
+				$(this).children().addClass("fa-minus");				
+			})
+		
     $(".desplegable").toggle(
           function() {
             $(".seccion-header").parent().find(".nav-menu").slideDown('');
@@ -14,4 +26,6 @@ $(window).resize(function(){
         $(".seccion-header").parent().find(".nav-menu").hide();
     }
 });
+
 });
+	
