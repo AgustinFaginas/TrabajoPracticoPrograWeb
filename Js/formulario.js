01d32555eda287e3d58b60df560adb8c76af7fc9
AgustinFaginas <agustin.faginas@hotmail.com>
2019-11-27 07:59:52 -0300

    Se hizo calendario y cambios en inscripcion

--- Js/formulario.js
+++ Js/formulario.js
@@ -1,14 +1,36 @@
+var precio=2000;
+
 $(document).ready(function(){
+    
+    
+    
 
     //clonar
     $(".clonar").click(function(){
+        precio=precio+2000;
+        $(".precioInscribirse").text(precio);
     	var clonar=$(this).parents(".contenedorCampos").clone(true); //clonar
         $(this).parent(".div-clonar-eliminar").parent(".contenedorCampos").after(clonar); //mostrar
+        
+          
+       
+        
+       
     });
+    $(".precioInscribirse").text(precio);
 
 
     //remover
     $(".eliminar").click(function(){
+        precio=precio-2000;
+        $(".precioInscribirse").text(precio);
     	$(this).parents(".contenedorCampos").remove();
     });
+    
+    $(".botonInscribirse").click(function(){
+        var nombres = "Felicitaciones  "+($("#nombre-A").val() +"  " + $("#apellido-A").val());
+        $("#modal").text(nombres);
+        
+    });
+    
 });
